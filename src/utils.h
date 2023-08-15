3acc72eb89f171c39007613afed71e0d65d1faf0
Macawls <josh@macawls.dev>
2023-08-15 13:38:36 +0200

    static for utils

--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,12 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <GLFW/glfw3.h>
+#include <stdio.h>
 
 /* Update the title of a GLFW window with the current frames per second (FPS). */
-void updateWindowTitle(GLFWwindow* window, int frameCount, double lastTime, const char* customTitle);
-
-#endif // UTILS_H
+static void updateWindowTitle(GLFWwindow* window, int frameCount, double lastTime, const char* customTitle) {
+    double currentTime = glfwGetTime();
+    if (currentTime - lastTime >= 1.0) {
+        double fps = (double)frameCount / (currentTime - lastTime);
+        char title[256];
+        snprintf(title, sizeof(title), "%s - %.2f FPS", customTitle, fps);
+        glfwSetWindowTitle(window, title);
+    }
+}
